dcb42b82cf783e8c5d0a3a10b7a3ebb091506f94
sanya midha <sanyamidha.10@gmail.com>
2022-01-11 22:35:28 +0530

    Product Variation and Variation Manager

--- carts/views.py
+++ carts/views.py
@@ -3,7 +3,7 @@ from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from carts.models import Cart, CartItem
 
-from store.models import Product
+from store.models import Product, Variation
 
 # Create your views here.
 def _cart_id(request):
@@ -14,6 +14,22 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    product_variation = []
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact = key, variation_value__iexact = value)
+                product_variation.append(variation)
+            except:
+                pass
+            
+
+
+    
     product = Product.objects.get(id=product_id)
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
